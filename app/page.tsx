358e1f0d4024b354f20f1cfe0bc76b93af0a8a89
Jad Bendarkawi <jadb@princeton.edu>
2023-08-18 23:43:55 -0400

    fix broken link

--- app/page.tsx
+++ app/page.tsx
@@ -267,7 +267,7 @@ export function Footer() {
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2023{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
+          <a href="https://jadbendarkawi.com/" className="hover:underline">
             jad bendarkawi
           </a>
           . All Rights Reserved.
